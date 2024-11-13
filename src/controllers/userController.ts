34833fc4233acc2cfd875f1dd710e4758d2b9030
JaneGH <jkrytsyna@gmail.com>
2024-11-13 12:50:32 -0800

    added swagger

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 
-const mainController = {
-  get: (req: Request, res: Response): Response => {
-    return res.json({
-      data: "This is a full stack app!",
-    });
-  },
+interface IUserRequestBody {
+  email: string;
+  password: string;
+  name?: string;
+}
+
+const register = async (
+  req: Request<{}, {}, IUserRequestBody>,
+  res: Response
+): Promise<void> => {};
+
+const login = async (
+  req: Request<{}, {}, IUserRequestBody>,
+  res: Response
+): Promise<void> => {
+  const { email, password } = req.body;
 };
 
-export default mainController;
+export { register, login };
